670a37cad07f39468a5dd2eef9f4a36be3597a1e
인오 강 <kanginoh@gmail.com>
2023-03-10 15:31:30 +0900

    ㅋㅋㅋㅋㅋㅋㅋ

--- IfElse/MainApp.cs
+++ IfElse/MainApp.cs
@@ -6,27 +6,39 @@ namespace IfElse
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("숫자를 입력하세요. : ");
+            Console.Write("숫자를 입력하세요. : ");
 
             string input = Console.ReadLine();
             int number = Convert.ToInt32(input);
             //int number = int.Parse(input);
 
-            if(number < 0)
+            if (number < 0)
+            {
                 Console.WriteLine("음수");
-
+            }
             else if (number > 0)
-                Console.WriteLine("양수");//
-
+            {
+                Console.WriteLine("양수");
+            }
             else
+            {
                 Console.WriteLine("0");
+            }
 
-            if (number % 2 == 0)
-                Console.WriteLine("짝수");
-            else
-                Console.WriteLine("홀수");
-                // Console.WriteLine("홀수");
-                // test
+            //if (number % 2 == 0)
+            //{
+            //    Console.WriteLine("짝수");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("홀수");
+            //}
+
+            string test;
+            test = number % 2 == 0 ? "짝수":"홀수";
+
+            Console.WriteLine(test);
+
 
         }
     }
---
+++ IfIf/MainApp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace IfIf
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("숫자를 입력하세요. : ");
+
+            string input = Console.ReadLine();  //40 / 11 / 0
+
+            int number = Convert.ToInt32(input);
+
+            if (number > 0)
+            {
+                if (number % 2 == 0)
+                    Console.WriteLine($"0보다 큰 짝수");  // 40  
+                else
+                    Console.WriteLine($"0보다 큰 홀수");  // 11
+            }
+            else
+            {
+                Console.WriteLine($"0보다 작거나 같은 수"); // 0
+            }    
+                
+           
+        }
+    }
+}
