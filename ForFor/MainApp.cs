bbf03a809a616e5c4593f25035a72c3183516865
인오 강 <kanginoh@gmail.com>
2023-03-15 17:56:20 +0900

    20230315집

---
+++ ForFor/MainApp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace ForFor
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            for (int x = 0; x < 5; x++)
+            {                
+                for (int y = 0; y <= x; y++) 
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine(); 
+            }
+        }
+    }
+}
