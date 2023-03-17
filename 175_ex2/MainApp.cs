a31fbf318e36bc8ff6679ebf77adbd38724dd3df
인오 강 <kanginoh@gmail.com>
2023-03-17 15:37:37 +0900

    20230307

---
+++ 175_ex2/MainApp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace p175_ex2
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+
+            int i = 1;
+
+            while (i>0)
+            {
+                Console.Write("중단하고 싶으시면 0을 눌러주세요. : ");
+                string t = Console.ReadLine();
+                int x = int.Parse(t);
+
+                x = i;
+            }
+        }
+    }
+}
